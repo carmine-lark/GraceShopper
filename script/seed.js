6c204f3edaf8a9b25c5eb6a7626de72e3ab73295
Conrad <theradistone@gmail.com>
2019-04-16 15:34:11 -0400

    Seeded Products

--- script/seed.js
+++ script/seed.js
@@ -3,6 +3,7 @@
 const db = require('../server/db')
 const {Product} = require('../server/db/models')
 
+
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
-const db = require('../db')
+const db = require('../db') 
+
 
 const Product = db.define('product', {
     name: {
